ac405812bc7a60cc195e4998acb3cce39bf7d37e
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-05-15 13:51:37 +0100

    feat: add xml for identity_node

--- targets/xml_writer.cpp
+++ targets/xml_writer.cpp
@@ -78,7 +78,7 @@ void mml::xml_writer::do_function_definition_node(mml::function_definition_node 
   closeTag(node, lvl);
 }
 void mml::xml_writer::do_identity_node(mml::identity_node * const node, int lvl) {
-  // EMPTY
+  do_unary_operation(node, lvl);
 }
 void mml::xml_writer::do_program_node(mml::program_node * const node, int lvl) {
   // EMPTY
