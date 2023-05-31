ab8f63b848af36e8d2291dfaa133b2e1fa593b6d
Pedro Lobo <pedro.lobo@tecnico.ulisboa.pt>
2023-05-31 16:05:34 +0100

    feat: implement sizeof_node postfix

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -60,7 +60,8 @@ void mml::postfix_writer::do_index_node(mml::index_node * const node, int lvl) {
   // EMPTY
 }
 void mml::postfix_writer::do_sizeof_node(mml::sizeof_node * const node, int lvl) {
-  // EMPTY
+  ASSERT_SAFE_EXPRESSIONS;
+  _pf.INT(node->expression()->type()->size());
 }
 void mml::postfix_writer::do_stack_alloc_node(mml::stack_alloc_node * const node, int lvl) {
   // EMPTY
